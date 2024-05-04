15f3596855a589f3fc00a94dca11e24b8d921e3f
AmmarAbdalfattah3 <ammarabdalfattah@gmail.com>
2024-05-03 19:51:13 -0700

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
-#This Bash script sets up server for deployment of web_static
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo service nginx start
-sudo mkdir -p /data/web_static/releases/test
+#a Bash script that configures my servers for the deployment of web_static
+
+sudo nginx -v
+cmd=$?
+if ! [ $cmd -eq 0 ]
+then
+	sudo apt-get -y update
+	sudo apt-get install -y nginx;
+fi
+sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo echo "Hello World!" >> /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
+echo "Hello World!" >> /data/web_static/releases/test/index.html
+sudo ls -l /data/web_static/current
+cmd=$?
+if [ $cmd -eq 0 ]
+then
+	sudo rm -rf /data/web_static/current
+fi
+
+sudo ln -sf  /data/web_static/releases/test/ /data/web_static/current
+
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+sudo sed -i "/server_name _;/a  location /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-enabled/default
 sudo service nginx restart
